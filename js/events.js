fff614a03bc00ba9d94007ad2fb7837604f30039
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-20 19:36:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,19 +1,32 @@
 function getIt() {
-$('p').on("click", function(click){
+$('p').on("click", function(){
   console.log ("Hey!")
 })
 };
 
 function frameIt() {
-
+$('img').on('load', function(){
+  //actions you want to happen
+})
 };
 
 function pressIt() {
-
+$(document).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+})
 };
 
 function submitIt() {
-
+  $("form").on("submit", function() {
+    if ($( "input:first" ).val() === "correct") {
+      alert('your form is going to be submitted now');
+      return;
+    }
+    alert("you entered the wrong value");
+    return;
+  });
 };
 
 $(document).ready(function(){
