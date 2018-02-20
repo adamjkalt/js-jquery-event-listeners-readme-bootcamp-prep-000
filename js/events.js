511cf030f49286f063413a02a4d0ae1d63ea70ae
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-20 19:41:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ $(document).on('keydown', function(key) {
 function submitIt() {
   $("form").on("submit", function() {
     if ($( "input:first" ).val() === "correct") {
-      alert('your form is going to be submitted now');
+      alert("Your form is going to be submitted now");
       return;
     }
     alert("you entered the wrong value");
