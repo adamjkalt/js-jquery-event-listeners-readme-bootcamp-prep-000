4b47fbb68b9ac50456875b7061304674b43ccf5f
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-20 19:43:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,6 @@ $(document).on('keydown', function(key) {
 function submitIt() {
   $("form").on("submit", function() {
       alert("Your form is going to be submitted now");
-      return;
   });
 };
 
